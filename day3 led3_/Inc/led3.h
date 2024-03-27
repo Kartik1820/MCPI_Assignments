d7c597e2550defcaa8feaab08169fe572991cdbb
Kartik <kartikgophane2408@gmail.com>
2024-03-27 23:15:29 +0530

    Day 3 2nd assignment

---
+++ day3 led3_/Inc/led3.h
@@ -0,0 +1,25 @@
+/*
+ * led3.h
+ *
+ *  Created on: Mar 27, 2024
+ *      Author: kartik
+ */
+
+#ifndef LED3_H_
+#define LED3_H_
+
+#include "stm32f407xx.h"
+
+#define LED_GPIO	GPIOD
+#define LED_GPIO_EN	3
+
+#define LED_GREEN	12
+#define LED_ORANGE	13
+#define LED_RED		14
+#define LED_BLUE	15
+
+void LedInit(uint32_t pin);
+void LedOn(uint32_t pin);
+void LedOff(uint32_t pin);
+//void LedBlink(uint32_t pin, uint32_t delay);
+#endif /* LED3_H_ */
